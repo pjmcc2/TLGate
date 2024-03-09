5b1fbe429b9e55bc88fa36d112e77ad2cd8171c4
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-09 15:40:29 -0800

    submit.sh

---
+++ submit.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+#
+#SBATCH --job-name=TLGate
+#SBATCH --partition=soundbendor
+#SBATCH -A soundbendor
+#SBATCH --gres=gpu:2
+#SBATCH -o sbatch_logs/main.out
+#SBATCH -e sbatch_logs/main.err
+
+module load slurm
+module load cuda/12.2
+source env/bin/activate
+python3 gate.py
