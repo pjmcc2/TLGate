ca361da8de849b7546c2e146bcceae24a3857866
Peter <pjmcc2@gmail.com>
2024-03-14 12:54:29 -0700

    kd.py

---
+++ KD.py
@@ -0,0 +1,187 @@
+from gate import train
+from gate import test
+from torch import nn
+import torch
+import numpy as np
+from torchvision.models import vgg13
+from tqdm import tqdm
+import pickle
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+from torchvision.transforms import Normalize
+import torch.optim as optim
+import argparse
+
+
+class WeightedAvg(nn.module):
+    def __init__(self):
+        super().__init__()
+        self.a = nn.Parameter(torch.rand((1, 1)).item())
+        self.a.requires_grad = True
+
+    def forward(self, l1, l2):
+        return self.a * l1 + (1 - self.a) * l2
+
+
+class studentNet(nn.Module):
+    def __init__(self, teacher, T):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1),  # 30,30,64
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 3, 1),  # 28,128
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, 2, padding=1),  # 14,128
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 14, 1),
+            nn.Flatten(),
+            nn.Linear(128, 1000),
+            nn.ReLU()
+        )
+
+        self.linear3 = nn.Linear(1000, 1000)
+        self.linear2 = nn.Linear(1000, 1000)
+        self.linear1 = nn.Linear(1000, 10)
+
+        self.teacher = teacher
+        self.T = T
+
+        def forward(self, X):
+            student_logits = self.feature_extractor(X)
+            teacher_logits = self.teacher(X)
+            teacher_prob = nn.functional.softmax(teacher_logits / self.T, dim=-1)
+            student_prob = nn.functional.softmax(student_logits / self.T, dim=-1)
+            return student_prob, teacher_prob
+
+
+## DISCLAIMER: THE FOLLOWING CODE IS MOSTLY COPIED FROM THE PYTORCH KD TUTORIAL ##
+def train_knowledge_distillation(teacher, student, train_loader, optim, loss_fn, T, a, device):
+    teacher.eval()  # Teacher set to evaluation mode
+    student.train()  # Student to train mode
+    optimizer = optim
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        # Forward pass with the teacher model - do not save gradients here as we do not change the teacher's weights
+        with torch.no_grad():
+            teacher_logits = teacher(inputs)
+
+        # Forward pass with the student model
+        student_logits = student(inputs)
+
+        # I am changing this forget the log.
+        ##Soften the student logits by applying softmax first and log() second
+        soft_targets = nn.functional.softmax(teacher_logits / T, dim=-1)
+        # soft_prob = nn.functional.log_softmax(student_logits / T, dim=-1)
+        soft_prob = nn.functional.softmax(student_logits / T, dim=-1)
+
+        # Calculate the soft targets loss. Scaled by T**2 as suggested by the authors of the paper "Distilling the knowledge in a neural network"
+        soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T ** 2)
+
+        # Calculate the true label loss
+        label_loss = loss_fn(student_logits, labels)
+
+        # Weighted sum of the two losses
+        loss = a * soft_targets_loss + (1 - a) * label_loss
+
+        loss.backward()
+        optimizer.step()
+
+
+def main(args):
+    T = args.Temperature
+    a = args.a
+    epochs = int(args.epochs)
+    num_tests = int(args.num_tests)
+    batch_size = int(args.batch_size)
+
+    train_data = CIFAR10(root="data",
+                         train=True,
+                         download=True,
+                         transform=Compose([ToTensor(), Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))]))
+
+    test_data = CIFAR10(root="data",
+                        train=False,
+                        download=True,
+                        transform=Compose([ToTensor(), Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))]))
+
+    batch_size = batch_size
+
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+
+    teacher = vgg13(weights='DEFAULT', progress=True)
+    teacher.to(device)
+    # Freeze layers
+    for param in teacher.parameters():
+        param.requires_grad = False
+
+    ### training part ###
+    epochs = epochs
+    num_tests = num_tests
+    criterion = nn.CrossEntropyLoss()
+
+    acc_lists_train = []
+    loss_lists_train = []
+    acc_lists_test = []
+    loss_lists_test = []
+    for i in range(num_tests):
+        model = ClassNet(teacher, -1)
+        model.to(device)
+        optimizer = optim.Adam(model.parameters(), lr=0.0001)
+        a_list_train = []
+        a_list_test = []
+        loss_list_train = []
+        loss_list_test = []
+
+        for epoch in range(epochs):
+            acc, loss = train(model, train_dataloader, criterion, optimizer, device)
+            a_list_train.append(acc)
+            loss_list_train.append(loss)
+
+            acc, loss = test(model, test_dataloader, criterion, device)
+
+            a_list_test.append(acc)
+            loss_list_test.append(loss)
+
+        acc_lists_test.append(a_list_test)
+        acc_lists_train.append(a_list_train)
+        loss_lists_test.append(loss_list_test)
+        loss_lists_train.append(loss_list_train)
+
+    tr_acc = np.array(acc_lists_train)
+    te_acc = np.array(acc_lists_test)
+    tr_loss = np.array(loss_lists_train)
+    te_loss = np.array(loss_lists_test)
+
+    save_dict = {"train_acc": tr_acc,
+                 "test_acc": te_acc,
+                 "train_loss": tr_loss,
+                 "test_loss": te_loss
+                 }
+    with open(f"pickles/gate_model_{gate_type}.pickle", "wb") as f:
+        pickle.dump(save_dict, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a KD model and pickle the results")
+    parser.add_argument("Temperature", help="Temperature: Softens the distribution")
+    parser.add_argument("a", help="weighted average parameter (for soft targets)")
+    parser.add_argument("epochs", help="Number of training epochs")
+    parser.add_argument("num_tests", help="number of training iterations")
+    parser.add_argument("batch_size", help="batch_size")
+
+    args = parser.parse_args()
+    main(args)
