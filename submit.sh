92570dc61aa130842f40ad72378e277f816a3d8f
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-17 19:45:01 -0700

    better modeling

--- submit.sh
+++ submit.sh
@@ -10,7 +10,11 @@
 module load slurm
 module load cuda/12.2
 source env/bin/activate
-python3 KD.py 6 0.6 10 10 512
-
-
-# 30 epochs, 10 tests, 512 batch size
+python3 KD.py 6 0.2 10 10 512
+python3 KD.py 6 0.4 10 10 512
+python3 KD.py 6 0.8 10 10 512
+python3 KD.py 4 0.5 10 10 512
+python3 KD.py 6 0.5 10 10 512
+python3 KD.py 8 0.5 10 10 512
+python3 KD.py 2 0.5 10 10 512
+# 10 epochs, 10 tests, 512 batch size
