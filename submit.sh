73b16c270da42cd93415a7b4897b2cf7ab2a9ee9
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-20 10:05:58 -0700

    batch

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=TLGate
-#SBATCH --partition=soundbendor
+#SBATCH --partition=soundbendor,dgxs,dgx2,dgxh
 #SBATCH -A soundbendor
 #SBATCH --gres=gpu:2
 #SBATCH -o sbatch_logs/main.out
@@ -10,7 +10,7 @@
 module load slurm
 module load cuda/12.2
 source genv/bin/activate
-python3 gate.py 0 1 1 512
+python3 gate.py 0 6 10 10 512
 
 # 10 epochs, 10 tests, 512 batch size
 #python3 gate.py 0 30 10 512
