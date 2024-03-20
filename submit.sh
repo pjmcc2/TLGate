1ecac85438c7dc981b1231dedd219b352a6f81eb
Peter McCabe <104118252+pjmcc2@users.noreply.github.com>
2024-03-20 11:10:40 -0700

    raw

--- submit.sh
+++ submit.sh
@@ -10,7 +10,7 @@
 module load slurm
 module load cuda/12.2
 source genv/bin/activate
-python3 gate.py 0 6 10 1 512
+python3 gate.py -1 1 10 1 512
 
 # 10 epochs, 10 tests, 512 batch size
 #python3 gate.py 0 30 10 512
